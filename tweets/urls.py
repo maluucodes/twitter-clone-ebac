e80b58ea5d67b4a19a60ac5950f92c10ff4ed691
Maria Luiza <m.luiza0020@gmail.com>
2026-07-02 17:36:06 -0300

    added tweet CRUD

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from tweets.views import feed_view
+from tweets.views import delete_tweet_view, edit_tweet_view, feed_view
 
 
 urlpatterns = [
     path("", feed_view, name="feed"),
+    path("tweets/<int:tweet_id>/editar/", edit_tweet_view, name="edit_tweet"),
+    path("tweets/<int:tweet_id>/deletar/", delete_tweet_view, name="delete_tweet"),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,7 +1,50 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+
+from tweets.forms import TweetForm
+from tweets.models import Tweet
 
 
 @login_required
 def feed_view(request):
-    return render(request, "tweets/feed.html")
+    tweets = Tweet.objects.all()
+
+    if request.method == "POST":
+        form = TweetForm(request.POST)
+
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.author = request.user
+            tweet.save()
+            return redirect("feed")
+    else:
+        form = TweetForm()
+
+    return render(request, "tweets/feed.html", {"tweets": tweets, "form": form})
+
+
+@login_required
+def edit_tweet_view(request, tweet_id):
+    tweet = get_object_or_404(Tweet, id=tweet_id, author=request.user)
+
+    if request.method == "POST":
+        form = TweetForm(request.POST, instance=tweet)
+
+        if form.is_valid():
+            form.save()
+            return redirect("feed")
+    else:
+        form = TweetForm(instance=tweet)
+
+    return render(request, "tweets/edit_tweet.html", {"form": form})
+
+
+@login_required
+def delete_tweet_view(request, tweet_id):
+    tweet = get_object_or_404(Tweet, id=tweet_id, author=request.user)
+
+    if request.method == "POST":
+        tweet.delete()
+        return redirect("feed")
+
+    return render(request, "tweets/delete_tweet.html", {"tweet": tweet})
